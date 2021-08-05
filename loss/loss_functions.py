0e06de7178909dfef152737e40470901f320a598
m-grbic <58181847+m-grbic@users.noreply.github.com>
2021-08-05 10:20:09 +0000

    add smooth_loss

--- loss/loss_functions.py
+++ loss/loss_functions.py
@@ -8,3 +8,24 @@ from time import time
 def l1_loss(gt_depth, depth):
     loss = (gt_depth - torch.squeeze(depth , 1)).abs().mean()
     return loss
+
+def smooth_loss(pred_map):
+    def gradient(pred):
+        D_dy = pred[:, :, 1:] - pred[:, :, :-1]
+        D_dx = pred[:, :, :, 1:] - pred[:, :, :, :-1]
+        return D_dx, D_dy
+
+    if type(pred_map) not in [tuple, list]:
+        pred_map = [pred_map]
+
+    loss = 0
+    weight = 1.
+
+    for scaled_map in pred_map:
+        #scaled_map=scaled_map.clamp(1e-3,80)
+        dx, dy = gradient(scaled_map)
+        dx2, dxdy = gradient(dx)
+        dydx, dy2 = gradient(dy)
+        loss += (dx2.abs().mean() + dxdy.abs().mean() + dydx.abs().mean() + dy2.abs().mean())*weight
+        weight /= 2.3  # don't ask me why it works better
+    return loss
